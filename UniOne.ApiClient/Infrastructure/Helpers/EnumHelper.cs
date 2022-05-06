b40e70cb56533ead65ed5f2e1978eb874ca19aef
usender <sharpsender@gmail.com>
2022-05-06 10:56:39 +0300

    downgrade net standard to 1.3

--- UniOne.ApiClient/Infrastructure/Helpers/EnumHelper.cs
+++ UniOne.ApiClient/Infrastructure/Helpers/EnumHelper.cs
@@ -7,11 +7,6 @@ namespace Sender.UniOne.ApiClient.Infrastructure.Helpers
     {
         internal static IEnumerable<T> GetValues<T>() where T : Enum
         {
-            if (!typeof(T).IsEnum)
-            {
-                throw new Exception("T must be enum.");
-            }
-
             var values = Enum.GetValues(typeof(T));
 
             foreach (var item in values)
